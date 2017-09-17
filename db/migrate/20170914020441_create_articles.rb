d5300f3ca18c55d17258b75587afb00add8b0437
dor4231 <dor4231@gmail.com>
2017-09-17 10:40:55 +0300

    Working blog features - Basic.

---
+++ db/migrate/20170914020441_create_articles.rb
@@ -0,0 +1,17 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      rescue => exception
+        
+      else
+        
+      ensure
+        
+      end
+
+      t.timestamps
+    end
+  end
+end
